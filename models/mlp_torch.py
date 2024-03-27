76fda433cad15e4c989e59661ba7094d68bb7af0
le-big-mac <charles.london@cs.ox.ac.uk>
2024-03-27 12:35:30 -0400

    training code

--- models/mlp_torch.py
+++ models/mlp_torch.py
@@ -1,10 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from copy import deepcopy
 
 class MFVI_NN(nn.Module):
